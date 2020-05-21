559cd45d339f9c52607dbb43409fb9a4dcb1457b
Ace Alrich <hex0@live.com>
2020-05-21 22:47:46 +0100

    create first object

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
-document.getElementById("titleH1").innerHTML = "What is going on";
+var canvas = document.getElementById("canvas1");
+var ctx = canvas.getContext("2d");
+ctx.canvas.width = window.innerWidth;
+ctx.canvas.height = window.innerHeight;
+function Particle(x, y, directionX, directionY, size, color) {
+    this.x = x;
+    this.y = y;
+    this.directionX = directionX;
+    this.directionY = directionY;
+    this.size = size;
+    this.color = color;
+}
+Particle.prototype.draw = function () {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2, false);
+    ctx.fillStyle - this.color;
+    ctx.fill();
+};
+var particle1 = new Particle(100, 100, 1, 1, 20, "white");
+particle1.draw();
+Particle.prototype.update = function () {
+    if (this.x + this.size > canvas.width || this.x - this.size < 0) {
+        this.directionX = -this.directionX;
+    }
+    if (this.y + this.size > canvas.width || this.y - this.size < 0) {
+        this.directionY = -this.directionY;
+    }
+    this.draw;
+};
--- main.ts
+++ main.ts
@@ -1,1 +1,32 @@
-document.getElementById("titleH1").innerHTML = "What is going on";
+const canvas: any = document.getElementById("canvas1");
+const ctx = canvas.getContext("2d");
+ctx.canvas.width = window.innerWidth;
+ctx.canvas.height = window.innerHeight;
+
+function Particle(x, y, directionX, directionY, size, color) {
+  this.x = x;
+  this.y = y;
+  this.directionX = directionX;
+  this.directionY = directionY;
+  this.size = size;
+  this.color = color;
+}
+
+Particle.prototype.draw = function () {
+  ctx.beginPath();
+  ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2, false);
+  ctx.fillStyle - this.color;
+  ctx.fill();
+};
+const particle1 = new Particle(100, 100, 1, 1, 20, "white");
+particle1.draw();
+
+Particle.prototype.update = function () {
+  if (this.x + this.size > canvas.width || this.x - this.size < 0) {
+    this.directionX = -this.directionX;
+  }
+  if (this.y + this.size > canvas.width || this.y - this.size < 0) {
+    this.directionY = -this.directionY;
+  }
+  this.draw;
+};
