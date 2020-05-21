f6f2a04aa6dbdf5ca1adc6d10b1c42662da87684
Ace Alrich <hex0@live.com>
2020-05-21 23:18:56 +0100

    give up

--- main.ts
+++ main.ts
@@ -3,6 +3,7 @@ const ctx = canvas.getContext("2d");
 ctx.canvas.width = window.innerWidth;
 ctx.canvas.height = window.innerHeight;
 
+const particleArray = [];
 function Particle(x, y, directionX, directionY, size, color) {
   this.x = x;
   this.y = y;
@@ -15,11 +16,9 @@ function Particle(x, y, directionX, directionY, size, color) {
 Particle.prototype.draw = function () {
   ctx.beginPath();
   ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2, false);
-  ctx.fillStyle - this.color;
+  ctx.fillStyle = this.color;
   ctx.fill();
 };
-const particle1 = new Particle(100, 100, 1, 1, 20, "white");
-particle1.draw();
 
 Particle.prototype.update = function () {
   if (this.x + this.size > canvas.width || this.x - this.size < 0) {
@@ -28,5 +27,30 @@ Particle.prototype.update = function () {
   if (this.y + this.size > canvas.width || this.y - this.size < 0) {
     this.directionY = -this.directionY;
   }
+  this.x += this.directionX;
+  this.y += this.directionY;
   this.draw;
 };
+
+function init() {
+  for (let i = 0; i < 100; i++) {
+    let size = Math.random() * 20;
+    let x = Math.random() * (innerWidth - size * 2);
+    let y = Math.random() * (innerHeight - size * 2);
+    let directionX = Math.random() * 0.4 - 0.2;
+    let directionY = Math.random() * 0.4 - 0.2;
+    let color = "white";
+    particleArray.push(new Particle(x, y, directionX, directionY, size, color));
+  }
+}
+
+function animate() {
+  requestAnimationFrame(animate);
+  ctx.clearRect(0, 0, innerWidth, innerHeight);
+  for (let i = 0; i < particleArray.length; i++) {
+    particleArray[i].update();
+  }
+}
+
+init();
+animate();
