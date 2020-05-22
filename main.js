cc3a790d8f28bd3811e39cde9f32dab6a4c15115
Ace Alrich <hex0@live.com>
2020-05-22 20:36:59 +0100

    clouds

--- main.js
+++ main.js
@@ -1,56 +1,94 @@
-var canvas = document.getElementById("canvas1");
-var ctx = canvas.getContext("2d");
+let canvas = document.getElementById("canvas1");
+let ctx = canvas.getContext("2d");
 ctx.canvas.width = window.innerWidth;
 ctx.canvas.height = window.outerHeight;
-var particleArray;
+let particleArray = [];
+const colours = [
+  "white",
+  "rgba(255,255,255,0.3",
+  "rgba(173,216,230,0.8",
+  "rgba(211,211,211,0.8",
+];
+const maxSize = 40;
+const minSize = 0;
+const mouseRadius = 60;
+
+let mouse = {
+  x: null,
+  y: null,
+};
+
+window.addEventListener("mousemove", (e) => {
+  mouse.x = e.x;
+  mouse.y = e.y;
+  console.log(mouse);
+});
 function Particle(x, y, directionX, directionY, size, color) {
-    this.x = x;
-    this.y = y;
-    this.directionX = directionX;
-    this.directionY = directionY;
-    this.size = size;
-    this.color = color;
+  this.x = x;
+  this.y = y;
+  this.directionX = directionX;
+  this.directionY = directionY;
+  this.size = size;
+  this.color = color;
 }
 Particle.prototype.draw = function () {
-    ctx.beginPath();
-    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2, false);
-    ctx.fillStyle = this.color;
-    ctx.fill();
+  ctx.beginPath();
+  ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2, false);
+  ctx.fillStyle = this.color;
+  ctx.fill();
 };
 Particle.prototype.update = function () {
-    if (this.x + this.size > canvas.width || this.x - this.size < 0) {
-        this.directionX = -this.directionX;
+  if (this.x + this.size * 2 > canvas.width || this.x - this.size * 2 < 0) {
+    this.directionX = -this.directionX;
+  }
+  if (this.y + this.size * 2 > canvas.width || this.y - this.size * 2 < 0) {
+    this.directionY = -this.directionY;
+  }
+  this.x += this.directionX;
+  this.y += this.directionY;
+  if (
+    mouse.x - this.x < mouseRadius &&
+    mouse.x - this.x > -mouseRadius &&
+    mouse.y - this.y < mouseRadius &&
+    mouse.y - this.y > -mouseRadius
+  ) {
+    if (this.size < maxSize) {
+      this.size += 3;
     }
-    if (this.y + this.size > canvas.width || this.y - this.size < 0) {
-        this.directionY = -this.directionY;
-    }
-    this.x += this.directionX;
-    this.y += this.directionY;
-    this.draw();
+  } else if (this.size > minSize) {
+    this.size -= 0.1;
+  }
+  if (this.size < 0) {
+    this.size = 0;
+  }
+  this.draw();
 };
 function init() {
-    particleArray = [];
-    for (var i = 0; i < 200; i++) {
-        var size = Math.random() * 10;
-        var x = Math.random() * (innerWidth - size * 2);
-        var y = Math.random() * (innerHeight - size * 2);
-        var directionX = Math.random() * 2 - 0.4;
-        var directionY = Math.random() * 1 - 0.4;
-        var color = "white";
-        particleArray.push(new Particle(x, y, directionX, directionY, size, color));
-    }
+  particleArray = [];
+  for (let i = 0; i < 1000; i++) {
+    let size = 0;
+    let x = Math.random() * (innerWidth - size * 2 - size * 2) + size * 2;
+    let y = Math.random() * (innerHeight - size * 2 - size * 2) + size * 2;
+    let directionX = Math.random() * 0.2 - 0.1;
+    let directionY = Math.random() * 0.2 - 0.1;
+    let colour = colours[Math.floor(Math.random() * colours.length)];
+    particleArray.push(
+      new Particle(x, y, directionX, directionY, size, colour)
+    );
+  }
 }
 function animate() {
-    requestAnimationFrame(animate);
-    ctx.clearRect(0, 0, innerWidth, innerHeight);
-    for (var i = 0; i < particleArray.length; i++) {
-        particleArray[i].update();
-    }
+  requestAnimationFrame(animate);
+  ctx.clearRect(0, 0, innerWidth, innerHeight);
+  for (let i = 0; i < particleArray.length; i++) {
+    particleArray[i].update();
+  }
 }
 init();
 animate();
+
 window.addEventListener("resize", function () {
-    canvas.width = this.innerWidth;
-    canvas.height = this.innerHeight;
-    init();
+  canvas.width = this.innerWidth;
+  canvas.height = this.innerHeight;
+  init();
 });
