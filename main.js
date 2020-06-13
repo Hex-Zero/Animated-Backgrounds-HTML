add506ab4394a57153ddc22141be8620cbeff5d9
Ace Alrich <hex0@live.com>
2020-06-13 14:08:51 +0100

    better controls

--- main.js
+++ main.js
@@ -1,17 +1,31 @@
 let amountValue;
 let percentValue;
 let periodValue;
+let percentInput = document.getElementById("percent-input");
+let percentLabel = document.getElementById("percent-label");
+
+const percentChange = () => {
+  percentLabel.innerHTML = `Percent ${percentInput.value} %`;
+};
+percentChange();
+percentInput.addEventListener("input", () => percentChange());
+
+function numberWithCommas(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
 
 const intrest = () => {
   amountValue = parseInt(document.getElementById("amount-input").value);
-  percentValue = parseInt(document.getElementById("percent-input").value);
+  percentValue = parseFloat(percentInput.value);
   periodValue = parseInt(document.getElementById("period-input").value);
   let result = amountValue;
   for (let i = 0; i < periodValue; i++) {
     result += (result / 100) * percentValue;
   }
-  document.getElementById("result").innerHTML = Math.floor(result) + " £";
+  document.getElementById("result").innerHTML =
+    numberWithCommas(Math.floor(result)) + " £";
 };
+console.log(parseInt(document.getElementById("percent-input").value));
 
 let calculate = document
   .getElementById("calculate-button")
