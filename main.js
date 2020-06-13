589161ce17f17ba22e834dfff9da9ea66fc17bc6
Ace Alrich <hex0@live.com>
2020-06-13 13:02:08 +0100

    done

--- main.js
+++ main.js
@@ -1,17 +1,18 @@
-const intrest = (amount, percent, period) => {
-  let result = amount;
-  for (let i = 0; i < period; i++) {
-    result += (result / 100) * percent;
+let amountValue;
+let percentValue;
+let periodValue;
+
+const intrest = () => {
+  amountValue = parseInt(document.getElementById("amount-input").value);
+  percentValue = parseInt(document.getElementById("percent-input").value);
+  periodValue = parseInt(document.getElementById("period-input").value);
+  let result = amountValue;
+  for (let i = 0; i < periodValue; i++) {
+    result += (result / 100) * percentValue;
   }
-  console.log(result);
+  document.getElementById("result").innerHTML = Math.floor(result) + " £";
 };
 
-let amountValue = document.getElementById("amount-input").value;
-let percentValue = document.getElementById("percent-input").value;
-let periodValue = document.getElementById("period-input").value;
-
-console.log(amountValue);
-console.log(percentValue);
-console.log(periodValue);
-
-intrest(300, 3, 300);
+let calculate = document
+  .getElementById("calculate-button")
+  .addEventListener("click", () => intrest());
